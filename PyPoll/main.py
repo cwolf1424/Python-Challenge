c1b32e7818e18f680db9ebea7e52ee3969ae7792
Caleb_Wolf <cwolf1424@gmail.com>
2023-08-06 17:53:50 -0500

    Ballot header broken into sections

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,22 +9,31 @@ data_file_path = "PyPoll/Resources/election_data.csv"
 total_votes = 0
 candidates = []
 candidate_votes = []
-vote_sections = []
+header = []
 
 #open results
 with open (data_file_path) as results:
+    #read results
     results_reader=csv.reader(results)
-    for vote in results:
+    for vote in results_reader:
+        #find header categories
+        if (results_reader.line_num ==1):
+            header = vote
+            print ("Ballot Sections:")
+            for section in header:
+                print (f'[{header.index(section)}]',section)
+            #remove header from vote count
+            total_votes -=1
         total_votes +=1
-    print (total_votes)
-#read results
+    
+
 
 #print results
 
 ##print ("Election Results")
-##print ("-------------------------")
-##print ("Total Votes: <votes_total")
-##print ("-------------------------")
+    print ("-------------------------")
+    print (f'Total Votes: {total_votes}')
+    print ("-------------------------")
 ##print ("<cand1_name>: <cand1_percent>% (<cand1_vote_count>)")
 ##print ("-------------------------")
 ##print ("Winner: <winner_name")
