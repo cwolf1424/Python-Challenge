65fac213fbfce0bc66e42e94008012344fd60076
Caleb_Wolf <cwolf1424@gmail.com>
2023-08-06 16:49:00 -0500

    Proto-code for PyPoll

---
+++ PyPoll/main.py
@@ -0,0 +1,16 @@
+#modules
+
+#read csv file
+
+#print results
+
+print ("Election Results")
+print ("-------------------------")
+print ("Total Votes: <votes_total")
+print ("-------------------------")
+print ("<cand1_name>: <cand1_percent>% (<cand1_vote_count>)")
+print ("-------------------------")
+print ("Winner: <winner_name")
+print ("------------------------")
+
+#export results
